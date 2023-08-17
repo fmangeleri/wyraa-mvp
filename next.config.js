d4ba7b9d683e203e72e28fc8a044af01d19d57f1
Franco <fmangeleri@gmail.com>
2023-08-17 12:50:58 -0300

    npm i

--- next.config.js
+++ next.config.js
@@ -3,22 +3,6 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.node$/,
-  //     use: 'node-loader',
-  //   });
-
-  // return config;
-  // },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.node/,
-  //     use: 'raw-loader',
-  //   });
-
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
